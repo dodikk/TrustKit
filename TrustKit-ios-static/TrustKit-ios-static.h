5fe85c05d1f1d2beffcb3e9cd69e374513529a83
dodikk <dodikk88.reg@gmail.com>
2016-10-17 20:20:08 +0300

    [static] added static framework target
    Unable to replace a binary in a dynamic framework since Xcode still
    tries to use dyld with it

---
+++ TrustKit-ios-static/TrustKit-ios-static.h
@@ -0,0 +1,19 @@
+//
+//  TrustKit-ios-static.h
+//  TrustKit-ios-static
+//
+//  Created by Oleksandr Dodatko on 17/10/2016.
+//  Copyright © 2016 TrustKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TrustKit-ios-static.
+FOUNDATION_EXPORT double TrustKit_ios_staticVersionNumber;
+
+//! Project version string for TrustKit-ios-static.
+FOUNDATION_EXPORT const unsigned char TrustKit_ios_staticVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrustKit_ios_static/PublicHeader.h>
+
+
